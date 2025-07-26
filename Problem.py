2f3bf535fc4b74d466137f542f695ea0fef0722a
SAFIR jHanm🔥 <121437369+TUNISIA-user@users.noreply.github.com>
2025-07-26 15:25:32 +0100

    Update Problem.py

--- Problem.py
+++ Problem.py
@@ -46,6 +46,13 @@ def firstMissingPositive(nums):
 
 
 
+def function__sort__positive(nums):
+    for i in range(1, len(nums)):
+        if nums[i] > 0:
+            j = i
+            while j > 0 and nums[j-1] > 0 and nums[j] < nums[j-1]:
+                nums[j], nums[j-1] = nums[j-1], nums[j]
+                j -= 1
 
 
 
